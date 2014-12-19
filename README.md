cb8edd2fddf07f2bfdb774bce6e73f3748a2aebd
Bill <billsearle@me.com>
2014-12-19 00:34:07 +0000

    Replaced colors for a lighter color

---
+++ README.md
@@ -0,0 +1,3 @@
+# New Tab Color Clock
+Background color is dirived from the current time, updating every second and fading to the next hex coded color.
+0, 1 and 2 have been replaced with a, b and c to give a lighter color.
--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 function colorClock () {
 	// get time
 	var newDate = new Date();
-	var hours = newDate.getHours();
-	var minutes = newDate.getMinutes();
-	var seconds = newDate.getSeconds();
+	var hours = newDate.getHours().toString();
+	var minutes = newDate.getMinutes().toString();
+	var seconds = newDate.getSeconds().toString();
+	
 	// format time to HHMMSS
 	if (hours < 10) { hours = "0" + hours };
 	if (minutes < 10) { minutes = "0" + minutes };
 	if (seconds < 10) { seconds = "0" + seconds };
 	var time = hours +":"+ minutes +":"+ seconds;
-	// create # color code
-	var hex = '#' + hours + minutes + seconds;
+	
+	// create dark (1-1) # color code
+	var darkColors = hours + minutes + seconds;
+	
+	// replace dark # numbers with lighter characters
+	var lightColors = darkColors.replace(/0/g, 'a');
+	lightColors = lightColors.replace(/1/g, 'b');
+	lightColors = lightColors.replace(/2/g, 'c');
+
 	// update the time on the page
 	document.getElementById("time").innerHTML = time;
+	document.getElementById("hex").innerHTML = '#' + lightColors;
+	
 	// fade the background color
-	Velocity( document.getElementsByTagName("body"), { backgroundColor: hex }, 1000);
+	Velocity( document.getElementsByTagName("body"), { backgroundColor: '#' + lightColors }, 1000);
 };
 
 
@@ -22,4 +32,13 @@ setInterval(function(){
 	colorClock();
 },1000);
 
+// toggle between time and hex
+$('#time, #hex').click(function(){
+	$('#time').toggle();
+	$('#hex').toggle();
+});
+
 colorClock();
+
+
+
